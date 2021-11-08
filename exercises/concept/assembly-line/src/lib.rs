1734466d75209a87593ac35b5beaf30e0a04c58a
mprotasevich <maksim.protasevich@gmail.com>
2021-11-08 22:26:52 +0300

    Solved exercises: hello-world reverse-string lucians-luscious-lasagna gigasecond assembly-line semi-structured-logs

--- exercises/concept/assembly-line/src/lib.rs
+++ exercises/concept/assembly-line/src/lib.rs
@@ -3,9 +3,18 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let produced_cars = speed as u64 * 221u64;
+
+    let success_rate = match speed {
+        0..=4  => 1.0,
+        5..=8  => 0.9,
+        9..=10 => 0.77,
+        _ => unreachable!("Speed can't be higher than 10. But speed = {}", speed)
+    };
+
+    produced_cars as f64 * success_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / 60f64) as u32
 }
--- exercises/concept/semi-structured-logs/src/lib.rs
+++ exercises/concept/semi-structured-logs/src/lib.rs
@@ -1,6 +1,6 @@
 // This stub file contains items which aren't used yet; feel free to remove this module attribute
 // to enable stricter warnings.
-#![allow(unused)]
+use std::fmt::{ Display, Formatter, Result };
 
 /// various log levels
 #[derive(Clone, PartialEq, Debug)]
@@ -9,16 +9,28 @@ pub enum LogLevel {
     Warning,
     Error,
 }
+
+impl Display for LogLevel {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        use LogLevel::*;
+        match *self {
+            Info => write!(f, "INFO"),
+            Warning => write!(f, "WARNING"),
+            Error => write!(f, "ERROR"),
+        }
+    }
+}
+
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!()
+    format!("[{}]: {}", level, message)
 }
 pub fn info(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Info, message)
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Warning, message)
 }
 pub fn error(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Error, message)
 }
