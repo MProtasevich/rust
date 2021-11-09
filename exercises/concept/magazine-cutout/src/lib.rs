54f7bdc9dc952ca37c269bbe03ecb9ef840f177f
mprotasevich <maksim.protasevich@gmail.com>
2021-11-09 12:35:36 +0300

    Solved exercise 'magazine-cutout'

--- exercises/concept/magazine-cutout/src/lib.rs
+++ exercises/concept/magazine-cutout/src/lib.rs
@@ -1,9 +1,30 @@
 // This stub file contains items which aren't used yet; feel free to remove this module attribute
 // to enable stricter warnings.
-#![allow(unused)]
 
-use std::collections::HashMap;
+use std::collections::{
+    HashMap,
+    hash_map::Entry::{ Occupied, Vacant }
+};
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    fn fold_function<'a>(mut acc: HashMap<&'a str, i32>, elem: &&'a str) -> HashMap<&'a str, i32> {
+        acc.entry(elem)
+            .and_modify(|e| { *e += 1 })
+            .or_insert(1);
+        acc
+    }
+
+    let mut dictionary = magazine.iter()
+        .fold(HashMap::new(), fold_function);
+
+    for word in note {
+        let entry = dictionary.entry(word).and_modify(|e| { *e -= 1 });
+        match entry {
+            Occupied(value) if value.get() < &0 => return false,
+            Vacant(_) => return false,
+            _ => continue
+        }
+    }
+
+    true
 }
