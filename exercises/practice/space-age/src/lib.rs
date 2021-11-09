9a3202e101db43c3a11630d390838d369e506282
mprotasevich <maksim.protasevich@gmail.com>
2021-11-09 11:30:04 +0300

    Solved exercise 'space-age'

--- exercises/practice/space-age/src/lib.rs
+++ exercises/practice/space-age/src/lib.rs
@@ -2,37 +2,38 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration(f64);
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        Duration(s as f64)
     }
 }
+
+const SECONDS_PER_YEAR: f64 = 31557600.0;
 
 pub trait Planet {
+    const PERIOD: f64;
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
+        d.0 / SECONDS_PER_YEAR / Self::PERIOD
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! planet {
+    ($planet_name:ident, $period:expr) => {
+        pub struct $planet_name;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+        impl Planet for $planet_name {
+            const PERIOD: f64 = $period;
+        }
+    }
+}
+
+planet!(Mercury, 0.2408467);
+planet!(Venus,   0.61519726);
+planet!(Earth,   1.0);
+planet!(Mars,    1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn,  29.447498);
+planet!(Uranus,  84.016846);
+planet!(Neptune, 164.79132);
