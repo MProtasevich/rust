ad75a461bb295569b288f5d0f310a08addf3c33f
mprotasevich <maksim.protasevich@gmail.com>
2021-11-09 11:40:57 +0300

    Solved exercise 'sublist'

--- exercises/practice/sublist/src/lib.rs
+++ exercises/practice/sublist/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering::{ Equal, Less, Greater };
+
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
@@ -6,6 +8,14 @@ pub enum Comparison {
     Unequal,
 }
 
+fn contains_all_of<T: PartialEq>(first: &[T], second: &[T]) -> bool {
+    second.is_empty() || first.windows(second.len()).any(|window| window == second)
+}
+
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    match first.len().cmp(second.len()) {
+        Equal   => if first == second                { Comparison::Equal }     else { Comparison::Unequal },
+        Less    => if contains_all_of(second, first) { Comparison::Sublist }   else { Comparison::Unequal },
+        Greater => if contains_all_of(first, second) { Comparison::Superlist } else { Comparison::Unequal }
+    }
 }
