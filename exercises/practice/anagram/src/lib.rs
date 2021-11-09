b988223fcbc5b1c7c19a6029154b567f8de7c36a
mprotasevich <maksim.protasevich@gmail.com>
2021-11-09 11:15:03 +0300

    Solved exercise 'anagram'

--- exercises/practice/anagram/src/lib.rs
+++ exercises/practice/anagram/src/lib.rs
@@ -1,9 +1,16 @@
-use std::collections::HashSet;
+use std::collections::{ HashSet, HashMap };
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    fn hash_word(word: &str) -> HashMap<char, u16> {
+        word.to_lowercase().chars().fold(HashMap::new(), |mut dict, chr| {
+            dict.entry(chr).and_modify(|e| { *e += 1 }).or_insert(1);
+            dict
+        })
+    }
+    let hashed_word = hash_word(word);
+    possible_anagrams.iter()
+        .filter(|anagram| anagram.to_lowercase() != word.to_lowercase())
+        .filter(|anagram| hash_word(anagram) == hashed_word)
+        .map(|&string| string)
+        .collect()
 }
