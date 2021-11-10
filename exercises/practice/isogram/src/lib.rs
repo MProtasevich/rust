d4d15e5888c1d117b3e424625dceb383b444dc6a
mprotasevich <maksim.protasevich@gmail.com>
2021-11-10 19:47:20 +0300

    Solved exercise 'isogram'

--- exercises/practice/isogram/src/lib.rs
+++ exercises/practice/isogram/src/lib.rs
@@ -1,3 +1,10 @@
+use std::collections::HashSet;
+
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    let candidate = candidate.chars()
+        .filter(|c| c.is_alphabetic())
+        .map(|c| c.to_ascii_lowercase())
+        .collect::<Vec<_>>();
+    let set = candidate.iter().collect::<HashSet<_>>();
+    set.len() == candidate.len()
 }
