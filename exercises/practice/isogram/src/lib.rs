50e5e8b1bd71631b9a0ac273391b61a052c8aa45
mprotasevich <maksim.protasevich@gmail.com>
2021-11-10 19:53:19 +0300

    Update exercise 'isogram'

--- exercises/practice/isogram/src/lib.rs
+++ exercises/practice/isogram/src/lib.rs
@@ -1,10 +1,8 @@
 use std::collections::HashSet;
 
 pub fn check(candidate: &str) -> bool {
-    let candidate = candidate.chars()
+    let mut set = HashSet::new();
+    candidate.to_ascii_lowercase().chars()
         .filter(|c| c.is_alphabetic())
-        .map(|c| c.to_ascii_lowercase())
-        .collect::<Vec<_>>();
-    let set = candidate.iter().collect::<HashSet<_>>();
-    set.len() == candidate.len()
+        .all(|c| set.insert(c))
 }
