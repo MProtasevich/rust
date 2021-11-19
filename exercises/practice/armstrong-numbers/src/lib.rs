81a09a6f15a790ff7e0a16377cbe07c5a163a280
mprotasevich <maksim.protasevich@gmail.com>
2021-11-19 10:16:14 +0300

    Solved exercise 'armstrong-numbers'

--- exercises/practice/armstrong-numbers/src/lib.rs
+++ exercises/practice/armstrong-numbers/src/lib.rs
@@ -1,3 +1,6 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let num_of_digits = ((num as f64).log10().floor() + 1.0) as u32;
+    num == (0..num_of_digits)
+        .map(|i| ((num / 10u32.pow(i)) % 10).pow(num_of_digits))
+        .sum::<u32>()
 }
