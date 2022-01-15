6fea0073f61c0358e73e65b076c2582c25a45312
mprotasevich <maksim.protasevich@gmail.com>
2022-01-15 21:53:43 +0300

    Solved exercise 'proverb'

--- exercises/practice/proverb/src/lib.rs
+++ exercises/practice/proverb/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    if list.is_empty() {
+        return String::new();
+    }
+
+    list.windows(2)
+        .map(|words| format!("For want of a {} the {} was lost.\n", words[0], words[1]))
+        .chain(std::iter::once(format!("And all for the want of a {}.", list[0])))
+        .collect()
 }
