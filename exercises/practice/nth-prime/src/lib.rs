e0cedc354082278946e714ec794a56a0e3295243
mprotasevich <maksim.protasevich@gmail.com>
2022-01-15 20:34:12 +0300

    Solved exercise 'nth-prime'

--- exercises/practice/nth-prime/src/lib.rs
+++ exercises/practice/nth-prime/src/lib.rs
@@ -1,3 +1,16 @@
+fn is_prime(n: u32) -> bool {
+    let prime_edge = (n as f64).sqrt().floor() as u32;
+    n % 2 != 0 && (3..=prime_edge).step_by(2).all(|divider| n % divider != 0)
+}
+
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    if n == 0 {
+        2
+    } else {
+        (3..)
+            .step_by(2)
+            .filter(|&n| is_prime(n))
+            .take(n as usize)
+            .last().unwrap()
+    }
 }
