14f4ec1b5566fa894ff0effd9c1dce56a9baa9d9
mprotasevich <maksim.protasevich@gmail.com>
2022-01-15 21:23:59 +0300

    Solved exercise 'prime-factors'

--- exercises/practice/prime-factors/src/lib.rs
+++ exercises/practice/prime-factors/src/lib.rs
@@ -1,3 +1,20 @@
+use std::iter::{once, successors };
+
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut prime_factors = vec![];
+    let mut number = n;
+    let factors = once(2).chain(successors(Some(3), |n: &u64| n.checked_add(2)))
+        .take_while(|&factor| factor <= n);
+
+    for factor in factors {
+        while number % factor == 0 {
+            prime_factors.push(factor);
+            number /= factor;
+        }
+        if number == 1 {
+            break;
+        }
+    }
+
+    prime_factors
 }
