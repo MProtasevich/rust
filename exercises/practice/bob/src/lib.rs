1344a34ada3b57f454f7ccae59edca0b9067bda6
mprotasevich <maksim.protasevich@gmail.com>
2022-01-20 09:29:04 +0300

    Solved exercise 'bob'

--- exercises/practice/bob/src/lib.rs
+++ exercises/practice/bob/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let message = message.trim();
+
+    let is_question = message.ends_with('?');
+    let yelling = !message.is_empty() &&
+                    message.chars().any(|chr| chr.is_ascii_alphabetic()) &&
+                    message == message.to_uppercase();
+
+    match (is_question, yelling, message.is_empty()) {
+        (true, false, _) => "Sure.",
+        (false, true, _) => "Whoa, chill out!",
+        (true, true, _)  => "Calm down, I know what I'm doing!",
+        (_, _, true)     => "Fine. Be that way!",
+        (_, _, _)        => "Whatever.",
+    }
 }
