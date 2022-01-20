41dab6ed905249199e777c37a8a06058e2d4320e
mprotasevich <maksim.protasevich@gmail.com>
2022-01-20 09:20:29 +0300

    Solved exercise 'raindrops'

--- exercises/practice/raindrops/src/lib.rs
+++ exercises/practice/raindrops/src/lib.rs
@@ -1,3 +1,18 @@
+const DROPS: [(u32, &'static str); 3] = [
+    (3, "Pling"),
+    (5, "Plang"),
+    (7, "Plong"),
+];
+
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let res: String = DROPS.iter()
+        .filter(|&&(num, _)| n % num == 0)
+        .map(|&(_, drop)| drop)
+        .collect();
+
+    if res.is_empty() {
+        n.to_string()
+    } else {
+        res
+    }
 }
