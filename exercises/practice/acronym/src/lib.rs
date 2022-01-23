05430a4f7c5c9883ddbd97fc55942338d1ef074f
mprotasevich <maksim.protasevich@gmail.com>
2022-01-23 21:11:11 +0300

    Solved exercise 'acronym'

--- exercises/practice/acronym/src/lib.rs
+++ exercises/practice/acronym/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    phrase.split(&[' ', '-', '_'][..])
+        .flat_map(|word| vec![
+            word.chars().nth(0),
+            word.chars().skip(1).skip_while(|chr| chr.is_uppercase()).find(|chr| chr.is_uppercase())
+        ].into_iter())
+        .filter_map(std::convert::identity)
+        .collect::<String>()
+        .to_uppercase()
 }
