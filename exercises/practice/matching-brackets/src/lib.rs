ceb2e06305cf2f4dcdb214dec9ab30e031f1bfd0
mprotasevich <maksim.protasevich@gmail.com>
2022-01-23 21:07:25 +0300

    Solved exercise 'matching-brackets'

--- exercises/practice/matching-brackets/src/lib.rs
+++ exercises/practice/matching-brackets/src/lib.rs
@@ -1,6 +1,14 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut stack = Vec::with_capacity(string.len() / 2);
+    for chr in string.chars() {
+        let prev_char = stack.last();
+        match (prev_char, chr) {
+            (_, c) if !"{}[]()".contains(c) => (),
+            (_, '{') | (_, '(') | (_, '[') => stack.push(chr),
+            (Some('{'), '}') | (Some('('), ')') | (Some('['), ']') => { stack.pop(); () },
+            (_, '}' | ')' | ']') => return false,
+            _ => (),
+        };
+    }
+    stack.is_empty()
 }
