75b7bcfc061846e56fa4cc400c2994391485f9f4
mprotasevich <maksim.protasevich@gmail.com>
2022-01-23 21:24:13 +0300

    Solved exercise 'allergies'

--- exercises/practice/allergies/src/lib.rs
+++ exercises/practice/allergies/src/lib.rs
@@ -1,33 +1,35 @@
-pub struct Allergies;
+use Allergen::*;
 
-#[derive(Debug, PartialEq)]
+pub struct Allergies(u32);
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+    Eggs = 1 << 0,
+    Peanuts = 1 << 1,
+    Shellfish = 1 << 2,
+    Strawberries = 1 << 3,
+    Tomatoes = 1 << 4,
+    Chocolate = 1 << 5,
+    Pollen = 1 << 6,
+    Cats = 1 << 7,
 }
+
+const ALLERGIES: [Allergen; 8] = [Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats];
 
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        unimplemented!(
-            "Given the '{}' score, construct a new Allergies struct.",
-            score
-        );
+        Allergies(score % 256)
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        unimplemented!(
-            "Determine if the patient is allergic to the '{:?}' allergen.",
-            allergen
-        );
+        let &Allergies(score) = self;
+        score & (*allergen as u32) != 0
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        ALLERGIES.iter()
+            .filter(|allergen| self.is_allergic_to(allergen))
+            .cloned()
+            .collect()
     }
 }
