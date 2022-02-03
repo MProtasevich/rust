fb05807e2cd76c953938f4f70c58a01bb6084571
mprotasevich <maksim.protasevich@gmail.com>
2022-02-03 22:22:03 +0300

    Solved exercise 'dot-dsl'

--- exercises/practice/dot-dsl/src/lib.rs
+++ exercises/practice/dot-dsl/src/lib.rs
@@ -1,9 +1,3 @@
-pub mod graph {
-    pub struct Graph;
-
-    impl Graph {
-        pub fn new() -> Self {
-            unimplemented!("Construct a new Graph struct.");
-        }
-    }
-}
+mod edge;
+mod node;
+pub mod graph;
