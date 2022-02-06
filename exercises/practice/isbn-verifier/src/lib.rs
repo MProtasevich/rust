88128804a6b4939605dcb3e7caa1a28999832d5a
mprotasevich <maksim.protasevich@gmail.com>
2022-02-06 23:20:05 +0300

    Solved exercise 'isbn-verifier'

--- exercises/practice/isbn-verifier/src/lib.rs
+++ exercises/practice/isbn-verifier/src/lib.rs
@@ -1,4 +1,21 @@
+const RADIX: u32 = 10;
+
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    unimplemented!("Is {:?} a valid ISBN number?", isbn);
+    let isbn_digits = isbn.chars()
+        .filter(|&chr| chr.is_digit(RADIX) || chr == 'X');
+
+    let digits_count = isbn_digits.clone().count();
+    let x_found = isbn.find('X');
+    if digits_count != 10 || (x_found != None && x_found.unwrap() != isbn.len() - 1) {
+        return false;
+    }
+
+    let hash_sum = isbn_digits
+        .map(|chr| if chr == 'X' { 10 } else { chr.to_digit(RADIX).unwrap() as usize })
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (multiplier, x)| acc + x * (multiplier + 1));
+
+    hash_sum % 11 == 0
 }
