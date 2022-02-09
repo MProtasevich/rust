56f9048bb26336e038b3870dbb875a2476c93a24
mprotasevich <maksim.protasevich@gmail.com>
2022-02-09 22:35:00 +0300

    Solved exercise 'perfect-numbers'

--- exercises/practice/perfect-numbers/src/lib.rs
+++ exercises/practice/perfect-numbers/src/lib.rs
@@ -1,3 +1,6 @@
+use Classification::{ Deficient, Perfect, Abundant };
+use std::cmp::Ordering::{ Less, Equal, Greater };
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Classification {
     Abundant,
@@ -6,5 +9,19 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    if num == 0 {
+        return None;
+    }
+
+    let sum_of_divisors: u64 = (1..=(num / 2))
+        .filter(|&divisor| num % divisor == 0)
+        .sum();
+
+    let classification = match sum_of_divisors.cmp(&num) {
+        Less => Deficient,
+        Equal => Perfect,
+        Greater => Abundant,
+    };
+
+    Some(classification)
 }
