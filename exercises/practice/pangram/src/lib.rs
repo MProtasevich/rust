a0fffb85350fa5fa19dff5c7c7259ef3a50d3a05
mprotasevich <maksim.protasevich@gmail.com>
2022-02-09 22:27:14 +0300

    Solved exercise 'pangram'

--- exercises/practice/pangram/src/lib.rs
+++ exercises/practice/pangram/src/lib.rs
@@ -1,4 +1,11 @@
+use std::collections::HashSet;
+
+const NUMBER_OF_LATIN_LETTERS: usize = 26;
+
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+    sentence.chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .map(|c| c.to_ascii_lowercase())
+        .collect::<HashSet<_>>().len() == NUMBER_OF_LATIN_LETTERS
 }
