c3508e4ce61393b2ba97d63115fdd8cb7fb3ba3f
mprotasevich <maksim.protasevich@gmail.com>
2022-02-14 22:39:21 +0300

    Solved exercise 'queen-attack'

--- exercises/practice/queen-attack/src/lib.rs
+++ exercises/practice/queen-attack/src/lib.rs
@@ -1,31 +1,32 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    position: ChessPosition,
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return None.",
-            rank,
-            file
-        );
+        if (0..8).contains(&rank) && (0..8).contains(&file) {
+            Some(ChessPosition { rank, file })
+        } else {
+            None
+        }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!(
-            "Given the chess position {:?}, construct a Queen struct.",
-            position
-        );
+        Queen { position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!(
-            "Determine if this Queen can attack the other Queen {:?}",
-            other
-        );
+        let rank_diff = self.position.rank - other.position.rank;
+        let file_diff = self.position.file - other.position.file;
+        rank_diff * file_diff == 0 || rank_diff.abs() == file_diff.abs()
     }
 }
