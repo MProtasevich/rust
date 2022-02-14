98fc0523a8eb6cff593a67c34e2f56504ed327b3
mprotasevich <maksim.protasevich@gmail.com>
2022-02-14 22:42:36 +0300

    Solved exercise 'rna-transcription'

--- exercises/practice/rna-transcription/src/lib.rs
+++ exercises/practice/rna-transcription/src/lib.rs
@@ -1,21 +1,71 @@
 #[derive(Debug, PartialEq)]
-pub struct Dna;
+enum DnaNucleotides {
+    Adenine,
+    Cytosine,
+    Guanine,
+    Thymine,
+}
 
 #[derive(Debug, PartialEq)]
-pub struct Rna;
+pub struct Dna {
+    seq: Vec<DnaNucleotides>,
+}
+
+#[derive(Debug, PartialEq)]
+enum RnaNucleotides {
+    Adenine,
+    Cytosine,
+    Guanine,
+    Uracil,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Rna {
+    seq: Vec<RnaNucleotides>,
+}
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        unimplemented!("Construct new Dna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", dna);
+        let mut nucleotide_seq = Vec::with_capacity(dna.len());
+
+        for (idx, chr) in dna.chars().enumerate() {
+            let nucleotide = match chr {
+                'A' => DnaNucleotides::Adenine,
+                'C' => DnaNucleotides::Cytosine,
+                'G' => DnaNucleotides::Guanine,
+                'T' => DnaNucleotides::Thymine,
+                _ => return Err(idx)
+            };
+            nucleotide_seq.push(nucleotide);
+        }
+        Ok(Dna { seq: nucleotide_seq })
     }
 
     pub fn into_rna(self) -> Rna {
-        unimplemented!("Transform Dna {:?} into corresponding Rna", self);
+        let seq = self.seq.iter().map(|dna_nucleotide| match dna_nucleotide {
+            DnaNucleotides::Adenine => RnaNucleotides::Uracil,
+            DnaNucleotides::Cytosine => RnaNucleotides::Guanine,
+            DnaNucleotides::Guanine => RnaNucleotides::Cytosine,
+            DnaNucleotides::Thymine => RnaNucleotides::Adenine,
+        }).collect();
+        Rna { seq }
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        unimplemented!("Construct new Rna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", rna);
+        let mut nucleotide_seq = Vec::with_capacity(rna.len());
+
+        for (idx, chr) in rna.chars().enumerate() {
+            let nucleotide = match chr {
+                'A' => RnaNucleotides::Adenine,
+                'C' => RnaNucleotides::Cytosine,
+                'G' => RnaNucleotides::Guanine,
+                'U' => RnaNucleotides::Uracil,
+                _ => return Err(idx)
+            };
+            nucleotide_seq.push(nucleotide);
+        }
+        Ok(Rna { seq: nucleotide_seq })
     }
 }
