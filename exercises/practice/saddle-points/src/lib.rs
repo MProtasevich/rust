9fb5205f0a2b040ea9097d67ae9914ecca43b509
mprotasevich <maksim.protasevich@gmail.com>
2022-02-14 22:45:02 +0300

    Solved exercise 'saddle-points'

--- exercises/practice/saddle-points/src/lib.rs
+++ exercises/practice/saddle-points/src/lib.rs
@@ -1,6 +1,13 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut res = Vec::new();
+    for (i, row) in input.iter().enumerate() {
+        for (j, cell_value) in row.iter().cloned().enumerate() {
+            let smallest_in_a_column = input.iter().all(|row| cell_value <= row[j]);
+            let greatest_in_a_row = row.iter().all(|&cell| cell_value >= cell);
+            if smallest_in_a_column && greatest_in_a_row {
+                res.push((i, j));
+            }
+        }
+    }
+    res
 }
