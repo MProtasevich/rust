e895658702e467e2072987f152496cb172b08c44
mprotasevich <maksim.protasevich@gmail.com>
2022-02-15 08:13:50 +0300

    Solved exercise 'run-length-encoding'

--- exercises/practice/run-length-encoding/src/lib.rs
+++ exercises/practice/run-length-encoding/src/lib.rs
@@ -1,7 +1,36 @@
+use std::str::FromStr;
+
 pub fn encode(source: &str) -> String {
-    unimplemented!("Return the run-length encoding of {}.", source);
+    let mut encoded_string: String = String::new();
+
+    let mut occurences = 1;
+    let mut peekable = source.chars().peekable();
+
+    while let Some(chr) = peekable.next() {
+        match peekable.peek() {
+            Some(&next_char) if chr == next_char => occurences += 1,
+            Some(_) | None => {
+                encoded_string.push_str(format_substring(chr, occurences).as_str());
+                occurences = 1;
+            }
+        }
+    }
+
+    encoded_string
+}
+
+fn format_substring(chr: char, occurrences: u32) -> String {
+    if occurrences > 1 {
+        format!("{}{}", occurrences, chr)
+    } else {
+        chr.to_string()
+    }
 }
 
 pub fn decode(source: &str) -> String {
-    unimplemented!("Return the run-length decoding of {}.", source);
+    source.split_inclusive(|ch: char| ch.is_alphabetic() || ch.is_ascii_whitespace())
+        .map(|substring| (substring.strip_suffix(|ch: char| ch.is_alphabetic() || ch.is_ascii_whitespace()), substring.chars().last()))
+        .map(|(count, ch)| (count.map(usize::from_str).unwrap().unwrap_or(1), ch.unwrap()))
+        .map(|(count, ch)| std::iter::repeat(ch).take(count).collect::<String>())
+        .collect()
 }
